9ad167d1c747902c21dc97d34c86e46c0e478400
Stevy-aimery <stevyaimery@gmail.com>
2024-06-03 05:26:41 +0100

    Design seeCompte and order v1

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
 
-class OrderPage extends StatelessWidget {
+class OrderPage extends StatefulWidget {
   final List<Map<String, String>> cartItems;
 
   const OrderPage({super.key, required this.cartItems});
 
+  @override
+  _OrderPageState createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Confirmation of purchase'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Purchase made successfully !!'),
-              Text('Delivery in 3 hours to the pergola'),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Navigate back to the previous page
-                  Navigator.pop(context);
-                },
-                child: Text('Back to product details'),
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Purchase made successfully !!'),
+            Text('Delivery in 3 hours to the pergola'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate back to the previous page
+                Navigator.pop(context);
+              },
+              child: Text('Back to product details'),
+            ),
+          ],
         ),
       ),
     );
